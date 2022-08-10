edbb0008c87850df05505d2ec50a8e49f59f0593
stonefulstm <stoneful.stm@gmail.com>
2022-08-10 16:41:40 -0300

    Requisito 10 - Adicionar botões para alterar a quantidade e remover produto do carrinho

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -14,8 +14,36 @@ export default class Carrinho extends Component {
     this.setState({ productCart: getSave });
   }
 
+  updateQuantity = (product, operation) => {
+    const { quantity } = product;
+    const { productCart } = this.state;
+    const cart = productCart;
+    let qtde = parseInt(quantity, 10);
+    if (operation === '+') {
+      qtde += 1;
+    } else {
+      qtde = qtde === 1 ? qtde : qtde - 1;
+    }
+    const produto = cart.filter((prod) => prod.id === product.id);
+    produto[0].quantity = qtde;
+    const carrinho = [...(cart.filter((prod) => prod.id !== product.id)), produto[0]];
+    this.setState({
+      productCart: carrinho,
+    });
+  }
+
+  removeProduct = (product) => {
+    const { productCart } = this.state;
+    const cart = productCart;
+    const carrinho = cart.filter((prod) => prod.id !== product.id);
+    this.setState({
+      productCart: carrinho,
+    });
+  }
+
   render() {
     const { productCart } = this.state;
+    console.log(productCart);
     return (
       <section>
         {productCart === null ? (
@@ -28,7 +56,36 @@ export default class Carrinho extends Component {
                     {product.title}
                   </p>
                   <p>{product.price}</p>
-                  <p data-testid="shopping-cart-product-quantity">1</p>
+                  <p data-testid="shopping-cart-product-quantity">
+                    {
+                      product.quantity
+                    }
+
+                  </p>
+                  <div>
+                    <button
+                      data-testid="product-decrease-quantity"
+                      type="button"
+                      onClick={ () => this.updateQuantity(product, '-') }
+                    >
+                      -
+                    </button>
+
+                    <button
+                      data-testid="product-increase-quantity"
+                      type="button"
+                      onClick={ () => this.updateQuantity(product, '+') }
+                    >
+                      +
+                    </button>
+                    <button
+                      data-testid="remove-product"
+                      type="button"
+                      onClick={ () => this.removeProduct(product) }
+                    >
+                      X
+                    </button>
+                  </div>
                 </div>))}
             </div>)}
 
